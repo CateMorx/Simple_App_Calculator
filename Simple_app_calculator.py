ad5d9eca0be7b111a7daab3007fe0be97e46b269
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-05-06 22:41:42 +0800

    Psuedocodes

---
+++ Simple_app_calculator.py
@@ -0,0 +1,18 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 5
+#Create a Simple App Calculator 1.  The application will ask the user to choose one of the  four math operations (Addition, Subtraction, Multiplication and Division)
+#2.  The application will ask the user for two numbers 3.  Display the result 4. The application will ask if the user wants to try again or not.
+#5. If yes, repeat Step 1. 6. If no, Display “Thank you!” and the program will exit 7. Use Python Function and appropriate Exceptions to capture errors during runtime.
+
+#Imports necessary elements
+#Creates Method For Calculator
+# Displaying options for operation
+#Gets User Input For Operation
+#If Invalid input, shows error message and prompts user to enter again
+# Asking for the first number
+#If Invalid input, shows error message and prompts user to enter again
+# Asking for the second number
+# Performing the calculations
+# Asking user if they want to try again or not
+#Prints Thank you and closes the program 
+# Create the GUI
